8bcd0c17afc9bb025f3fd5f95b5eac07a6f0ea87
unknown <whijlp@outlook.com>
2025-06-03 15:26:46 -0500

    actulizacion del fetch

--- frontend/src/auth/auth.js
+++ frontend/src/auth/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3001/api";
+const BASE_URL = "https://api.alrededor.mooo.com";
 
 //"https://se-register-api.en.tripleten-services.com/v1"
 export const register = async (email, password) => {
